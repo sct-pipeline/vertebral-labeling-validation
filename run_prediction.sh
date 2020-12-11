c7e383a8ec24125aab2811824d834dec956d3d3c
lrouhier <lucas.rouhier@telecom-paristech.fr>
2020-12-11 13:48:10 -0500

    using python cmd

--- run_prediction.sh
+++ run_prediction.sh
@@ -18,6 +18,8 @@
 # Immediately exit if error
 
 set -e
+source ${SCT_DIR}/python/etc/profile.d/conda.sh
+conda activate venv_sct
 
 # Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
 trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
@@ -37,28 +39,31 @@ cp -r $PATH_DATA/$SUBJECT ./
 cp -r $PATH_DATA/derivatives/labels/$SUBJECT $PATH_DATA_PROCESSED/data/derivatives/labels
 
 cd $PATH_DATA_PROCESSED/$SUBJECT/anat/
-echo "file,error_mm,error_mse,contrast">> $PATH_RESULTS/"$SUBJECT"_result.csv
+echo "file,error_mm,label_missing,contrast">> $PATH_RESULTS/"$SUBJECT"_result.csv
 ## Setup file names
 contrast='T1 T2'
 for i in $contrast; do
 	file=${SUBJECT}_"$i"w
 	file_seg=$PATH_DATA_PROCESSED/data/derivatives/labels/$SUBJECT/anat/${SUBJECT}_"$i"w_seg.nii.gz
 	c_args=${i/T/t}
+	export SUBJECT=$SUBJECT
+	export file=$file
 
 	## make predictions
 	sct_label_vertebrae -i ${file}.nii.gz -s ${file_seg} -c "$c_args" -ofolder $PATH_DATA_PROCESSED/data/derivatives/labels/$SUBJECT/anat/
 
-## compare 
-	err=$(sct_label_utils -i $PATH_DATA_PROCESSED/data/derivatives/labels/$SUBJECT/anat/${file}_seg_labeled_discs.nii.gz -MSE $PATH_DATA_PROCESSED/data/derivatives/labels/$SUBJECT/anat/${file}_projected-gt.nii.gz)
+## compare
+       label_missing=$(python -c 'import os; from spinalcordtoolbox import labels as labels, image as image; print (labels.check_missing_label(image.Image(os.environ["PATH_DATA_PROCESSED"]+"/data/derivatives/labels/"+os.environ["SUBJECT"]+"/anat/"+ os.environ["file"]+"_seg_labeled_discs.nii.gz"),image.Image(os.environ["PATH_DATA_PROCESSED"]+"/data/derivatives/labels/"+os.environ["SUBJECT"]+"/anat/"+ os.environ["file"]+"_projected-gt.nii.gz")))')
+       err=$(python -c 'import os; from spinalcordtoolbox import labels as labels, image as image; print (labels.compute_mean_squared_error(image.Image(os.environ["PATH_DATA_PROCESSED"]+"/data/derivatives/labels/"+os.environ["SUBJECT"]+"/anat/"+ os.environ["file"]+"_seg_labeled_discs.nii.gz"),image.Image(os.environ["PATH_DATA_PROCESSED"]+"/data/derivatives/labels/"+os.environ["SUBJECT"]+"/anat/"+ os.environ["file"]+"_projected-gt.nii.gz")))')
 
 ## strip unneeded content
-	err=${err#*)}
+	##err=${err#*)}
 
 ## get error in mm
-	err_mm=${err#*=}
-	err_mm=${err_mm%%mm*}
+	##err_mm=${err#*=}
+	##err_mm=${err_mm%%mm*}
 
 ## add csv line with the error and contrast.
-	echo "$file,$err_mm,$c_args">>$PATH_RESULTS/"$SUBJECT"_result.csv
+	echo "$file,$err,$label_missing,$c_args">>$PATH_RESULTS/"$SUBJECT"_result.csv
 done
 
