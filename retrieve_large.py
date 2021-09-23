ef3a8b0ab620dae1a62fa321213f17d91a247c1b
Joshua Newton <joshuacwnewton@gmail.com>
2021-09-23 12:15:51 -0400

    `retrieve_large.py`: Fix bug by accounting for `ses` folders
    Before, `ses` subfolders would be missed because of hardcoded `anat` path

--- retrieve_large.py
+++ retrieve_large.py
@@ -4,20 +4,30 @@ import argparse
 import sys
 
 
+def get_anat_dir(sub_dir):
+    """
+    This function takes a subject directory and retrieves the first `anat` directory it finds. (This is needed
+    because sometimes the anat subdirectory is `sub-001/anat` and sometimes it's `sub-001/ses-001/anat`.)
+    
+    :return: subdirectory containing the string 'anat'
+    :rtype: str
+    """
+    for root, dirs, _ in os.walk(sub_dir):
+        for d in dirs:
+            if 'anat' in os.path.join(root, d):
+                return os.path.join(root, d)
+    raise FileNotFoundError(f"No 'anat' directory found for {sub_dir}")
+
+
 def main(path_file, path_data, path_output):
     os.makedirs(path_output, exist_ok = True)
     with open(path_file, 'r') as f:
         subject = f.readlines()
     for sub in subject:
         sub = sub.strip()
-        path_out_derivatives_tmp = os.path.join(path_output,'derivatives/labels/', sub, 'anat')
-        path_out_tmp = os.path.join(path_output, sub, 'anat/')
-        path_tmp_derivatives = os.path.join(path_data,'derivatives/labels/', sub, 'anat')
-        path_tmp = os.path.join(path_data, sub, 'anat/')
-        tmp = os.listdir(path_tmp_derivatives)
-        os.makedirs(path_out_derivatives_tmp, exist_ok=True)
-        os.makedirs(path_out_tmp, exist_ok=True)
-        for x in tmp:
+        path_in_derivatives = get_anat_dir(os.path.join(path_data, 'derivatives/labels/', sub))
+        path_in = get_anat_dir(os.path.join(path_data, sub))
+        for x in os.listdir(path_in_derivatives):
             if 'labels-disc-manual.nii.gz' in x:
                 name_im_in = x[:-26]+'.nii.gz'
                 if 't2' or 'T2' in x:
@@ -26,8 +36,15 @@ def main(path_file, path_data, path_output):
                 elif 't1' or 'T1' in x:
                     name_im_out = sub + '_T1w.nii.gz'
                     name_gt_out = sub + '_T1w_labels-disc-manual.nii.gz'
-                shutil.copy(os.path.join(path_tmp_derivatives,x),os.path.join(path_out_derivatives_tmp,name_gt_out))
-                shutil.copy(os.path.join(path_tmp,name_im_in), os.path.join(path_out_tmp,name_im_out))
+                print(f"Copying files for {sub}.")
+                path_out_derivatives_tmp = os.path.join(path_output, 'derivatives/labels/', sub, 'anat')
+                path_out_tmp = os.path.join(path_output, sub, 'anat')
+                os.makedirs(path_out_derivatives_tmp, exist_ok=True)
+                os.makedirs(path_out_tmp, exist_ok=True)
+                shutil.copy(os.path.join(path_in_derivatives, x),
+                            os.path.join(path_out_derivatives_tmp, name_gt_out))
+                shutil.copy(os.path.join(path_in, name_im_in),
+                            os.path.join(path_out_tmp, name_im_out))
 
 
 def get_parser():
